3014e2a1f31ecb1ba8ec6f0a1b4aebacf869e668
Eric A. Meyer <eric@meyerweb.com>
2015-05-26 13:20:38 -0400

    New test

--- css3/core.php
+++ css3/core.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($_SERVER["SERVER_NAME"] == 'mw') {
-	echo "PHP " . phpversion();
+	echo "<!-- PHP " . phpversion() . "-->";
 	error_reporting(E_ALL);
 	ini_set('display_errors', '1');
 }
